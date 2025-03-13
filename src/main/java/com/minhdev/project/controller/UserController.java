1ddaf70cd2de5033d0ae3d021b30f6e72112f1b5
xminh <kirito14042003@gmail.com>
2025-03-13 18:48:18 +0700

    crud user basic

--- src/main/java/com/minhdev/project/controller/UserController.java
+++ src/main/java/com/minhdev/project/controller/UserController.java
@@ -2,10 +2,9 @@ package com.minhdev.project.controller;
 
 import com.minhdev.project.domain.User;
 import com.minhdev.project.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,10 +14,30 @@ public class UserController {
         this.userService = userService;
     }
 
-//    @GetMapping("/user/create")
-    @PostMapping("/user/create")
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable long id) {
+        User devUser = this.userService.handleGetUser(id);
+        return devUser;
+    }
+
+    @GetMapping("/user")
+    public List<User> getUsers() {
+        return this.userService.handleGetAllUsers();
+    }
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User request) {
         User devUser = this.userService.handleCreateUser(request);
         return devUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable long id) {
+        this.userService.handleDeleteUser(id);
+        return "User deleted";
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User request) {
+        return this.userService.handleUpdateUser(request);
     }
 }
--- src/main/java/com/minhdev/project/service/UserService.java
+++ src/main/java/com/minhdev/project/service/UserService.java
@@ -4,6 +4,8 @@ import com.minhdev.project.domain.User;
 import com.minhdev.project.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -12,7 +14,31 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public User handleGetUser(long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> handleGetAllUsers() {
+        return this.userRepository.findAll();
+    }
+
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
+    }
+
+    public void handleDeleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public User handleUpdateUser(User user) {
+        long id = user.getId();
+        User updateUser = this.userRepository.findById(id).orElse(null);
+        if (updateUser != null) {
+            updateUser.setName(user.getName());
+            updateUser.setEmail(user.getEmail());
+            updateUser.setPassword(user.getPassword());
+            return this.userRepository.save(updateUser);
+        }
+        return null;
     }
 }
