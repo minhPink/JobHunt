0c814ba74fdbd32b34fd16fa55a96094e2deb6ed
xminh <kirito14042003@gmail.com>
2025-03-14 23:01:07 +0700

    format rest response

--- src/main/java/com/minhdev/project/controller/UserController.java
+++ src/main/java/com/minhdev/project/controller/UserController.java
@@ -2,6 +2,7 @@ package com.minhdev.project.controller;
 
 import com.minhdev.project.domain.User;
 import com.minhdev.project.service.UserService;
+import com.minhdev.project.service.error.IdInvalidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +34,10 @@ public class UserController {
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable long id) throws IdInvalidException {
+        if (id >= 1500) {
+            throw new IdInvalidException("Id khong duoc lon hon 1500");
+        }
         this.userService.handleDeleteUser(id);
         return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
     }
---
+++ src/main/java/com/minhdev/project/util/FormatRestResponse.java
@@ -0,0 +1,45 @@
+package com.minhdev.project.util;
+
+import com.minhdev.project.domain.RestResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+    private final HttpServletResponse httpServletResponse;
+
+    public FormatRestResponse(HttpServletResponse httpServletResponse) {
+        this.httpServletResponse = httpServletResponse;
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        int status = httpServletResponse.getStatus();
+
+        RestResponse<Object> restResponse = new RestResponse<Object>();
+        restResponse.setStatusCode(status);
+
+        if (body instanceof String) {
+            return body;
+        }
+
+        if (status >= 400) {
+            return body;
+        } else {
+            restResponse.setData(body);
+            restResponse.setMessage("Success");
+        }
+        return restResponse;
+    }
+}
