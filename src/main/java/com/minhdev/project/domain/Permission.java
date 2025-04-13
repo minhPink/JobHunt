7aef212685430205be1955e31c648f2b02689c70
xminh <kirito14042003@gmail.com>
2025-04-13 16:29:57 +0700

    model role and permission

---
+++ src/main/java/com/minhdev/project/domain/Permission.java
@@ -0,0 +1,58 @@
+package com.minhdev.project.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.minhdev.project.util.SecurityUtil;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+@Table(name = "permissions")
+@Getter
+@Setter
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name can't be blank")
+    private String name;
+
+    @NotBlank(message = "ApiPath can't be blank")
+    private String apiPath;
+
+    @NotBlank(message = "Method can't be blank")
+    private String method;
+
+    @NotBlank(message = "Module can't be blank")
+    private String module;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "permissions")
+    @JsonIgnore
+    private List<Role> roles;
+
+    @PrePersist
+    public void handleBeforePersist() {
+        this.createdAt = Instant.now();
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+    }
+
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedAt = Instant.now();
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+    }
+}
---
+++ src/main/java/com/minhdev/project/domain/Role.java
@@ -0,0 +1,53 @@
+package com.minhdev.project.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.minhdev.project.util.SecurityUtil;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name can't be blank")
+    private String name;
+
+    private String description;
+    private boolean active;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = {"roles"})
+    @JoinTable(name = "permission_role", joinColumns = @JoinColumn(name = "role_id"),
+    inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private List<Permission> permissions;
+
+    @PrePersist
+        public void handleBeforePersist() {
+            this.createdAt = Instant.now();
+            this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                    ? SecurityUtil.getCurrentUserLogin().get()
+                    : "";
+        }
+
+        @PreUpdate
+        public void handleBeforeUpdate() {
+            this.updatedAt = Instant.now();
+            this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                    ? SecurityUtil.getCurrentUserLogin().get()
+                    : "";
+    }
+}
